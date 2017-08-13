980ef836934e3d0aac49dd41385d9c20f867c2ae
林鈺家 <Annie@MacBook-Pro.local>
2017-08-13 16:01:57 +0800

    新增文章刪除功能

--- models/articlemodel.js
+++ models/articlemodel.js
@@ -3,7 +3,8 @@ var Schema = mongoose.Schema;
 var articleSchema = new Schema({
   articleTitleTable: String,
   articleBodyTable: String,
-  articleAuthor: String
+  articleAuthor: String,
+  articleID: String
 });
 
 module.exports = mongoose.model('Article', articleSchema);
--- routes/article.js
+++ routes/article.js
@@ -10,21 +10,28 @@ var jsonParser = bodyParser.json()
 // create application/x-www-form-urlencoded parser
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 // POST /login gets urlencoded bodies
-router.post('/addArticle', function (req, res, done) {
+router.post('/addArticle', function (req, res) {
     console.log('新增文章');
     var newArticle = new Article();
     newArticle.articleTitleTable = req.body.articleTitle;
     newArticle.articleBodyTable = req.body.articleBody;
+    newArticle.articleID = req.user.username;
     newArticle.articleAuthor = req.user.nickname;
     newArticle.save(function (err) {
     if (err) {  //angular,react,vue
         throw err;
     }
-    return done(null);
+        //return done(null); //Error: Can't set headers after they are sent. sent response end twice
     });
     // if (!req.body) return res.sendStatus(400)
     res.redirect('/home');
 })
-
+//刪除文章
+router.get('/deleteArticle/:article_id',function (req, res) {
+    console.log('刪除文章');
+    Article.findByIdAndRemove(req.params.article_id, function(err){
+    });
+    res.redirect('/home');
+})
 
 module.exports = router;
